615ba9bbad0cbd4bae84dd92ae713ff1334f4fa6
Johnny Miller <johnny@jpm-macbook.home>
2014-10-10 17:56:28 +0100

    Adding deb-proxy to cache packages

---
+++ Vagrant/dse/config/misc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing NTP"
+sudo apt-get install ntp
+echo ">>> Finished Installing NTP"
+
+echo ">>> Installing handy stuff"
+sudo apt-get install avahi-utils unzip vim curl zip python-software-properties python-pip git -y
+
+echo ">>> Installing GoLang and pcstat"
+sudo apt-get install golang -y
+
+sudo echo 'export GOPATH=/usr/share/go/' >> /etc/profile
+sudo echo 'export PATH=$PATH:$GOPATH/bin' >> /etc/profile
+sudo go get github.com/tobert/pcstat
+
+echo ">>> Adding OS tunings"
+sudo swapoff --all
+sudo echo "vm.max_map_count = 131072" >> /etc/sysctl.conf
+sudo sysctl -p
