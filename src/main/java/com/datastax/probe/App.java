b15b705acd3e52e23ecad5759c5b03fde91e7a04
Johnny Miller <johnny@jpm-macbook.local>
2014-10-30 17:00:18 +0100

    Switching to use commons-cli
    Arguments getting complicated

--- src/main/java/com/datastax/probe/App.java
+++ src/main/java/com/datastax/probe/App.java
@@ -1,5 +1,14 @@
 package com.datastax.probe;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -17,49 +26,77 @@ import com.datastax.probe.job.ProbeJob;
 public class App {
 
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    
+    @SuppressWarnings("static-access")
+    private static Options getCLiOption() {
+	Options options = new Options();
+	
+	Option interval = OptionBuilder.withArgName("int").withLongOpt("interval")
+		.isRequired(false)
+		.hasArg()
+		.withType(Integer.class)
+		.withDescription("OPTIONAL: Interval in seconds between probe jobs. Overlapping probe jobs will not occur. If not specified the probe will only run once and then exit")
+		.create();
+	options.addOption(interval);
+	
+	Option cassandraYaml = OptionBuilder.withArgName("string").withLongOpt("yaml")
+		.isRequired(true)
+		.hasArg()
+		.withType(String.class)
+		.withDescription("The path to the cassandra.yaml file")
+		.create();
+	options.addOption(cassandraYaml);
+	
+	Option cqlshRc = OptionBuilder.withArgName("string").withLongOpt("cqlshrc")
+		.isRequired(false)
+		.hasArg()
+		.withType(String.class)
+		.withDescription("OPTIONAL: The path to the CQLSHRC file containing security user credentials to connect to Cassandra")
+		.create();
+	options.addOption(cqlshRc);
+	
+	
+	return options;
+    }
 
     public static void main(String[] args) {
-	if (args == null || args.length < 2) {
-	    String message = "Invalid usage. Interval in seconds should be passed in as arg[0]. If only required to run once, set interval to < 1 seconds. Path to cassandra.yaml should be passed in as arg[1]. "
-		    + "Path to cqlshrc file should be (optionally) passed in as arg[2]";
-	    LOG.error(message);
-	    System.err.println(message);
+	
+	 CommandLineParser parser = new BasicParser();
+	 
+	 CommandLine cmd = null;
+	 try {
+	    cmd = parser.parse(App.getCLiOption(), args);
+	} catch (ParseException e) {
+	    String msg = "Problem encountered parsing command line arguments : "+e.getMessage();
+	    LOG.error(msg);
+	    System.err.println(msg);
+	    new HelpFormatter().printHelp( "java -jar cassandra-probe.jar", App.getCLiOption() );
 	    System.exit(1);
 	}
+	 
+	 
+	int interval = Integer.parseInt(cmd.getOptionValue("interval", "-1"));
+	LOG.info("interval: "+interval);
+	String yaml = cmd.getOptionValue("yaml");
+	LOG.info("yaml: "+yaml);
+	String cqlshrc = cmd.getOptionValue("cqlshrc");
 
-	int interval = -1;
-	String yamlPath = null;
-	try {
-	    interval = Integer.parseInt(args[0].trim());
-	    LOG.info("interval provided as '" + interval + "'");
-	    yamlPath = args[1].trim();
-	    LOG.info("yamlPath provided as '" + yamlPath + "'");
-	} catch (Exception e) {
-	    String msg = "Problem encountered parsing arguments: " + e.getMessage();
-	    LOG.error(msg, e);
-	    e.printStackTrace(System.err);
-	    System.exit(1);
-	}
-
-	String cqlshrcPath = null;
-	if (args.length == 3) {
-	    cqlshrcPath = args[2];
-	    LOG.info("cqlshrc path provided as '" + cqlshrcPath + "'");
+	if (StringUtils.isNotBlank(cqlshrc)) {
+	    LOG.info("cqlshrc path provided as '" + cqlshrc + "'");
 	} else {
 	    LOG.info("No cqlshrc path provided. Cassandra will be connected to without authentication");
-
 	}
 
 	try {
 	    if (interval < 1) {
 		LOG.info("Running probe once only");
-		final Prober app = (cqlshrcPath != null) ? new Prober(yamlPath, cqlshrcPath) : new Prober(yamlPath);
+		final Prober app = (cqlshrc != null) ? new Prober(yaml, cqlshrc) : new Prober(yaml);
 		app.probe();
 		System.exit(0);
 	    } else {
 		LOG.info("Running probe continuously with an interval of "+interval+" seconds between probes");
 		final App app = new App();
-		app.startJob(interval, yamlPath, cqlshrcPath);
+		app.startJob(interval, yaml, cqlshrc);
 	    }
 	} catch (Exception e) {
 	    String msg = "Problem encountered starting job: " + e.getMessage();
